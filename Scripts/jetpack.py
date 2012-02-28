538e2e3de9f1348babf08c7c390ad63ae95d98ea
snollygolly <snollygolly@localhost>
2012-02-27 20:34:46 -0600

    added toggle, more feedback, and tweaks to jetpack

--- Scripts/jetpack.py
+++ Scripts/jetpack.py
@@ -1,4 +1,4 @@
-from commands import add, admin, get_player
+from commands import add, admin, get_player, alias
 from pyspades.server import input_data
 from twisted.internet import reactor
 from pyspades import contained as loaders
@@ -6,13 +6,10 @@ from pyspades import contained as loaders
 FUEL_CAP = 60.0
 
 def jump_cycle(player, interval=0.05):
-    print "jump cycle start"
     if not player.world_object or not player.armed or not player.flying or player.fuel <=0:
         return
-    print "made jump cycle first check"
     world_object = player.world_object
     ##if world_object.acceleration.z == 0.0 and player.fuel > 0:
-    print "jumping!"
     world_object.jump = True
     input_data.player_id = player.player_id
     input_data.up = world_object.up
@@ -27,25 +24,28 @@ def jump_cycle(player, interval=0.05):
     player.fuel -= 1
     #end indent
     if world_object.position.z > 0.0:
-        print "not jumping"
         reactor.callLater(interval, jump_cycle, player)
     else:
-        print "done jumping"
         player.flying = False
 
 def charge_cycle(player, interval=0.10):
-    print "charge cycle start"
     if not player.world_object or player.flying or player.fuel >= FUEL_CAP:
         return
-    print "made charge cycle first check"
     player.fuel += 0.5
+    if player.fuel == FUEL_CAP:
+        print "fuel now full"
+        player.send_chat('Your fuel is now fully charged.')
     reactor.callLater(interval, charge_cycle, player)
 
 
-
+@alias('j')
 def jetpack(player):
-    print "jetpack, start"
-    player.armed = True
+    if player.armed == True:
+        player.armed = False
+    else:
+        player.armed = True
+    on_off = ['OFF', 'ON'][int(player.armed)]
+    return "Jetpack is now turned %s" % on_off
     #pos = player.world_object.position
     #z = player.protocol.map.get_z(pos.x, pos.y)
 add(jetpack)
@@ -55,21 +55,24 @@ def apply_script(protocol, connection, config):
     class JetConnection(connection):
         armed = False
         flying = False
-        fuel = 100
+        fuel = FUEL_CAP
     
+        def on_spawn(self, pos):
+            self.fuel = FUEL_CAP
+            return connection.on_spawn(self, pos)
+
         def on_animation_update(self, fire, jump, crouch, aim):
             if crouch == True:
-                print "anim update: jump true"
                 if self.armed == True and self.flying == False:
-                    print "anim update: turn on flying"
                     self.flying = True
                     jump_cycle(self)
             else:
                 if self.flying == True:
-                    print "anim update: stop flying"
                     self.flying = False
                     charge_cycle(self)
             
+            if self.fuel < 0:
+                self.send_chat('You are out of fuel! Wait for it to recharge.')
             return connection.on_animation_update(self, fire, jump, crouch, aim)
 
     return protocol, JetConnection
